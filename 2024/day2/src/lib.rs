892c4b7c4d21eb32827366d9d6fa797d12dfa4ff
Qazalbash <meesumqazalbash@gmail.com>
2024-12-13 01:41:05 +0500

    implement part2 solver for report validation and update tests

--- 2024/day2/src/lib.rs
+++ 2024/day2/src/lib.rs
@@ -1,13 +1,54 @@
 mod input;
 
 pub mod part1 {
+    pub fn solver(reports: Vec<Vec<i32>>) -> u16 {
+        reports
+            .iter()
+            .map(|report| {
+                let n: usize = report.len();
+                let is_positive: bool = report[n - 1] > report[0];
+                report.windows(2).all(move |w| {
+                    let diff = w[1] - w[0];
+                    !(0 == diff.abs() || diff.abs() > 3 || (is_positive ^ (diff > 0)))
+                })
+            })
+            .map(|x| match x {
+                true => 1_u16,
+                false => 0_u16,
+            })
+            .sum()
+    }
+}
+pub mod part2 {
     fn solve_a_report(report: &[i32]) -> u16 {
         let n: usize = report.len();
         let is_positive: bool = report[n - 1] > report[0];
         for i in 0..(n - 1) {
             let diff = report[i + 1] - report[i];
             if 0 == diff.abs() || diff.abs() > 3 || (is_positive ^ (diff > 0)) {
-                return 0_u16;
+                // remove ith index and try again
+                let mut new_report = report.to_vec();
+                new_report.remove(i);
+                let mut ith_flag = true;
+                for j in 0..(n - 2) {
+                    let diff = new_report[j + 1] - new_report[j];
+                    if 0 == diff.abs() || diff.abs() > 3 || (is_positive ^ (diff > 0)) {
+                        ith_flag = false;
+                        break;
+                    }
+                }
+                if !ith_flag {
+                    // remove ith+1 index and try again
+                    new_report = report.to_vec();
+                    new_report.remove(i + 1);
+                    for j in 0..(n - 2) {
+                        let diff = new_report[j + 1] - new_report[j];
+                        if 0 == diff.abs() || diff.abs() > 3 || (is_positive ^ (diff > 0)) {
+                            return 0_u16;
+                        }
+                    }
+                }
+                return 1_u16;
             }
         }
         1_u16
@@ -19,12 +60,6 @@ pub mod part1 {
             count += solve_a_report(report);
         }
         count
-    }
-}
-pub mod part2 {
-    /// Incomplete
-    pub fn solver(reports: Vec<Vec<i32>>) -> u16 {
-        0
     }
 }
 
@@ -46,6 +81,6 @@ mod tests {
     fn test2() {
         let reports = input::input();
         let ans = part2::solver(reports);
-        assert_eq!(ans, 524);
+        assert_eq!(ans, 569);
     }
 }
